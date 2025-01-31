c6eaaded747ac13cd66509c7abc3676c7df73985
Almar Louisse Inoferio <ialmar@yahoo.com>
2025-01-31 20:58:15 +0800

    last

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function showProfile() {
                 <strong>Hi! I'm Almar Louisse T. Inoferio</strong><br><br>
                 I'm a second year college student in Bachelor of Science in Computer Science at Mapua Malayan Colleges of Mindanao. I am 20 years old and slightly happy but I'm still persevering in order to be better. I'm a pure Filipino blood and proud, that is probably why I love supporting locals. My interests are listening to music, watching reality shows, scrolling through social media, playing games, and coding.
             </p>
-            <img src="asdsa.png" alt="Profile Image" />
+            <img src="images/asdsa.png" alt="Profile Image" />
         </div>
     `;
 }
@@ -16,16 +16,16 @@ function showProjects() {
     mainContent.innerHTML = `
         <div class="projects">
             <div class="project-item">
-                <img src="project1.png" alt="Project 1">
+                <img src="images/project1.png" alt="Project 1">
                 <button onclick="window.open('https://github.com/SavvSean/RentalContracts', '_blank')">Learn More</button>
             </div>
             <div class="project-item">
-                <img src="project2.png" alt="Project 2">
+                <img src="images/project2.png" alt="Project 2">
                 <button onclick="window.open('https://github.com/Xlai05/RestaurantReview', '_blank')">Learn More</button>
             </div>
             <div class="project-item">
-                <img src="project3.png" alt="Project 3">
-                <button onclick="window.open('HI.pdf', '_blank')">Learn More</button>
+                <img src="images/project3.png" alt="Project 3">
+                <button onclick="window.open('images/HI.pdf', '_blank')">Learn More</button>
             </div>
         </div>
     `;
@@ -37,15 +37,15 @@ function showCertification() {
             
             
             <div class="project-item">
-                <img src="c1.png" alt="Project 1">
+                <img src="images/c1.png" alt="Project 1">
                 
             </div>
             <div class="project-item">
-                <img src="c2.png" alt="Project 2">
+                <img src="images/c2.png" alt="Project 2">
                 
             </div>
             <div class="project-item">
-                <img src="c3.png" alt="Project 3">
+                <img src="images/c3.png" alt="Project 3">
                 
             </div>
 
@@ -62,19 +62,19 @@ function showContacts() {
             <ul>
                 <li>
                     <a href="https://www.instagram.com/al.louisse/" target="_blank">
-                        <img src="co3.png" alt="Contact 1" class="contact-icon">
+                        <img src="images/co3.png" alt="Contact 1" class="contact-icon">
                         Al.louisse
                     </a>
                 </li>
                 <li>
                     <a href="https://www.facebook.com/inoferio.Gwapo" target="_blank">
-                        <img src="co2.png" alt="Contact 2" class="contact-icon">
+                        <img src="images/co2.png" alt="Contact 2" class="contact-icon">
                         Almar Louisse Inoferio
                     </a>
                 </li>
                 <li>
                     <a href="tel:09498261802" target="_blank">
-                        <img src="co1.png" alt="Contact 3" class="contact-icon">
+                        <img src="images/co1.png" alt="Contact 3" class="contact-icon">
                         0949 826 1802 (Smart)
                     </a>
                 </li>
