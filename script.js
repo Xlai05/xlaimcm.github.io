c36d1eaca1afcb4d751c5219bce4eb181f1340bb
Almar Louisse Inoferio <ialmar@yahoo.com>
2025-01-31 23:18:14 +0800

    pinakalast

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ function showProfile() {
     mainContent.innerHTML = `
         <div class="profile">
             <p style="border: 2.5px solid #000; padding: 10px;">
-                <strong>Hi! I'm Almar Louisse T. Inoferio</strong><br><br>
-                I'm a second year college student in Bachelor of Science in Computer Science at Mapua Malayan Colleges of Mindanao. I am 20 years old and slightly happy but I'm still persevering in order to be better. I'm a pure Filipino blood and proud, that is probably why I love supporting locals. My interests are listening to music, watching reality shows, scrolling through social media, playing games, and coding.
+                <strong style="font-family: 'Brush Script MT', cursive; font-size: 24px;">Hi! I'm Almar Louisse T. Inoferio</strong><br><br>
+                <span style="font-family: Verdana, sans-serif;">
+                    I'm a second year college student in Bachelor of Science in Computer Science at Mapua Malayan Colleges of Mindanao. I am 20 years old and slightly happy but I'm still persevering in order to be better. I'm a pure Filipino blood and proud, that is probably why I love supporting locals. My interests are listening to music, watching reality shows, scrolling through social media, playing games, and coding.
+                </span>
             </p>
             <img src="images/asdasda.png" alt="Profile Image" />
         </div>
@@ -56,9 +58,9 @@ function showCertification() {
 function showContacts() {
     const mainContent = document.getElementById('main-content');
     mainContent.innerHTML = `
-        <div class="contacts">
+        <div class="contacts" style="font-family: Verdana, sans-serif;">
             <h2>Contact me @: </h2>
-            <ul style="border: 2.5px solid #000; padding: 10px;">
+            <ul style="border: 2px solid #000; padding: 10px;">
                 <li>
                     <a href="https://www.instagram.com/al.louisse/" target="_blank">
                         <img src="images/co3.png" alt="Contact 1" class="contact-icon">
